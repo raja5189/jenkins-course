796f8797a47db6109b38d5a68fb5ff91f88dc395
raja5189 <79685177+raja5189@users.noreply.github.com>
2021-06-03 21:56:43 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('raja5189 / example_nodejs_app')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
